ae5a5cf36fc6adcc82f5677285d539a5d823a2ae
Nam Tran <you@example.com>
2024-05-03 13:05:26 -0700

    first commit

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const NavbarActions = () => {
+    return (
+        <div className="ml-auto flex items-center gap-x-4">
+            
+        </div>
+    )
+}
+
+export default NavbarActions
